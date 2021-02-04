a31fe497200cb91fb159957342a36c104d5e9a89
alexoid <alex_zam@hotmail.es>
2021-02-04 14:19:30 -0500

    change config

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module BookstoreApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :options]
+         resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
        end
     end
     # Settings in config/environments/* take precedence over those specified here.
